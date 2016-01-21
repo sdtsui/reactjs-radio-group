85828dc966188ad8fddc38256eee42a89c8be91f
sdtsui <danielt213@gmail.com>
2016-01-20 19:07:56 -0800

    complete: 5/8 - listening to events

--- demo/components/RadioOtherOption.jsx
+++ demo/components/RadioOtherOption.jsx
@@ -1,15 +1,34 @@
 var RadioOtherOption = React.createClass({
+  getInitialState: function () {
+    return {
+      checked: false
+    };
+  },
+
+  onChange: function (event) {
+    var input = event.target;
+
+    console.log(event, input);
+    this.setState({
+      checked: input.checked
+    });
+
+  },
+
   render: function() {
     return (
       <p className="form-group radio">
         <label>
-          <input type="radio" name="referrer" value="other"/>
+          <input onChange={this.onChange} type="radio" name="referrer" value="other"/>
           Other
         </label>
-        <label className="form-inline">
-          Please specify:
-          <input type="text" name="referrer_other"/>
-        </label>
+
+        {this.state.checked && (
+          <label className="form-inline">
+            Please specify:
+            <input type="text" name="referrer_other"/>
+          </label>
+        )}
       </p>
       );
   }
